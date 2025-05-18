5d4d0f82047be6f9b8fb607ba112f54ef0baa373
iKedaTakumi07 <k024g0003@g.neec.ac.jp>
2025-05-18 23:16:49 +0900

    色描画するところまで

--- Object3d.PS.hlsl
+++ Object3d.PS.hlsl
@@ -1,11 +1,17 @@
-struct PixelShaderOutput
-{
-    float32_t4 color : SV_TARGET0;
+
+struct Material{
+    float32_t4 coler;
+};
+
+ConstantBuffer<Material> gMaterial: register(b0);
+struct PixelShaderOutput{
+   float32_t4 color:SV_TARGET0; 
 };
 
 PixelShaderOutput main()
 {
     PixelShaderOutput output;
-    output.color = float32_t4(1.0, 1.0, 1.0, 1.0);
+    output.color = gMaterial.coler;
     return output;
 }
+
--- main.cpp
+++ main.cpp
@@ -21,7 +21,34 @@
 #pragma comment(lib, "dxcompiler.lib")
 
 struct Vector4 {
-    float m[4];
+    float x;
+    float y;
+    float z;
+    float w;
+};
+
+ID3D12Resource* CreateBufferResource(ID3D12Device* device, size_t sizwInBytes)
+{
+    // 頂点リソース用のヒープを設定
+    D3D12_HEAP_PROPERTIES uploadHeapProperties {};
+    uploadHeapProperties.Type = D3D12_HEAP_TYPE_UPLOAD;
+    // 頂点リソースの設定
+    D3D12_RESOURCE_DESC vertexResourceDesc {};
+    // バッファリソース
+    vertexResourceDesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
+    vertexResourceDesc.Width = sizwInBytes;
+    // バッファの場合はこれらは1にする決まり
+    vertexResourceDesc.Height = 1;
+    vertexResourceDesc.DepthOrArraySize = 1;
+    vertexResourceDesc.MipLevels = 1;
+    vertexResourceDesc.MipLevels = 1;
+    vertexResourceDesc.SampleDesc.Count = 1;
+    // バッファの場合はこれにする決まり
+    vertexResourceDesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
+    // 実際に頂点リソースを作る
+    ID3D12Resource* vertexResource = nullptr;
+    device->CreateCommittedResource(&uploadHeapProperties, D3D12_HEAP_FLAG_NONE, &vertexResourceDesc, D3D12_RESOURCE_STATE_GENERIC_READ, nullptr, IID_PPV_ARGS(&vertexResource));
+    return vertexResource;
 };
 
 // CrashHandler
@@ -421,6 +448,15 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
     // RootSignature作成
     D3D12_ROOT_SIGNATURE_DESC descriptionRootSignature {};
     descriptionRootSignature.Flags = D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT;
+
+    // RootParameter作成
+    D3D12_ROOT_PARAMETER rootParameters[1] = {};
+    rootParameters[0].ParameterType = D3D12_ROOT_PARAMETER_TYPE_CBV;
+    rootParameters[0].ShaderVisibility = D3D12_SHADER_VISIBILITY_PIXEL;
+    rootParameters[0].Descriptor.ShaderRegister = 0;
+    descriptionRootSignature.pParameters = rootParameters;
+    descriptionRootSignature.NumParameters = _countof(rootParameters);
+
     // シリアスライズしてバイナリにする
     ID3DBlob* signatureBlob = nullptr;
     ID3DBlob* errorBlob = nullptr;
@@ -484,28 +520,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
     hr = device->CreateGraphicsPipelineState(&graphicsPipelineStateDesc, IID_PPV_ARGS(&graphicsPipelineState));
     assert(SUCCEEDED(hr));
 
-    // 頂点リソース用のヒープを設定
-    D3D12_HEAP_PROPERTIES uploadHeapProperties {};
-    uploadHeapProperties.Type = D3D12_HEAP_TYPE_UPLOAD;
-    // 頂点リソースの設定
-    D3D12_RESOURCE_DESC vertexResourceDesc {};
-    // バッファリソース
-    vertexResourceDesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
-    vertexResourceDesc.Width = sizeof(Vector4) * 3;
-    // バッファの場合はこれらは1にする決まり
-    vertexResourceDesc.Height = 1;
-    vertexResourceDesc.DepthOrArraySize = 1;
-    vertexResourceDesc.MipLevels = 1;
-    vertexResourceDesc.MipLevels = 1;
-    vertexResourceDesc.SampleDesc.Count = 1;
-    // バッファの場合はこれにする決まり
-    vertexResourceDesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
-    // 実際に頂点リソースを作る
-    ID3D12Resource* vertexResource = nullptr;
-    hr = device->CreateCommittedResource(&uploadHeapProperties, D3D12_HEAP_FLAG_NONE, &vertexResourceDesc, D3D12_RESOURCE_STATE_GENERIC_READ, nullptr, IID_PPV_ARGS(&vertexResource));
-    assert(SUCCEEDED(hr));
-
     // 頂点場合はびゅーを作成する
+    ID3D12Resource* vertexResource = CreateBufferResource(device, sizeof(Vector4) * 3);
+
     D3D12_VERTEX_BUFFER_VIEW vertexBufferView {};
     // リソースの先頭のアドレス使う
     vertexBufferView.BufferLocation = vertexResource->GetGPUVirtualAddress();
@@ -524,6 +541,15 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
     vertexData[1] = { 0.0f, 0.5f, 0.0f, 1.0f };
     // 右下
     vertexData[2] = { 0.5f, -0.5f, 0.0f, 1.0f };
+
+    // マテリアル用のリソースを作る
+    ID3D12Resource* materialResource = CreateBufferResource(device, sizeof(Vector4));
+    // マテリアルにデータを書き込む
+    Vector4* materialData = nullptr;
+    // 書き込むためのアドレスを取得
+    materialResource->Map(0, nullptr, reinterpret_cast<void**>(&materialData));
+    // 今回は赤を書き込んでみる
+    *materialData = Vector4(1.0f, 0.0f, 0.0f, 1.0f);
 
     // びゅーポート
     D3D12_VIEWPORT viewport {};
@@ -577,6 +603,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
             float clearColor[] = { 0.1f, 0.25f, 0.5f, 1.0f }; // 青っぽい色。RGBAの順番
             commandList->ClearRenderTargetView(rtvHandles[backBufferIndex], clearColor, 0, nullptr);
 
+            // 三角形
             commandList->RSSetViewports(1, &viewport);
             commandList->RSSetScissorRects(1, &scissorRect);
             // RootSignatureを設定。PSOに設定しているけど別途設定が必要
@@ -585,6 +612,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
             commandList->IASetVertexBuffers(0, 1, &vertexBufferView);
             // 形状を設定
             commandList->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+            // マテリアルCBuffrtの場所を設定
+            commandList->SetGraphicsRootConstantBufferView(0, materialResource->GetGPUVirtualAddress());
             // 描画
             commandList->DrawInstanced(3, 1, 0, 0);
 
@@ -651,6 +680,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
     rootSignature->Release();
     pixeShaderBlob->Release();
     vertexShaderBlob->Release();
+    materialResource->Release();
 #ifdef _DEBUG
     debugController->Release();
 #endif // _DEBUG
