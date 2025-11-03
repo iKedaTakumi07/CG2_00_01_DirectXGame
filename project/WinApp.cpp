8b583c66d3ca61b3e9d0f04404783ece48a8a51e
iKedaTakumi07 <k024g0003@g.neec.ac.jp>
2025-11-03 18:13:06 +0900

    該当箇所の移設

--- project/WinApp.cpp
+++ project/WinApp.cpp
@@ -2,6 +2,43 @@
 
 void WinApp::Initialize()
 {
+    HRESULT hr = CoInitializeEx(0, COINIT_MULTITHREADED);
+    WNDCLASS wc {};
+    // ウィンドウプロ―ジャ
+    wc.lpfnWndProc = Windowproc;
+    // ウィンドウクラス名(なんでもよし)
+    wc.lpszClassName = L"CG2WindowClass";
+    // インスタンスハンドル
+    wc.hInstance = GetModuleHandle(nullptr);
+    // カーソル
+    wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
+    // ウィンドウクラスを登録する
+    RegisterClass(&wc);
+
+    // クライアント領域のサイズ
+    const int32_t KClientWidth = 1280;
+    const int32_t KClientHeight = 720;
+
+    // ウィンドウサイズを表す構造体にクライアント領域を入れる
+    RECT wrc = { 0, 0, KClientWidth, KClientHeight };
+
+    // クライアント領域を元に実際のサイズをwrcを変更してもらう
+    AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
+
+    // ウィンドウの作成
+    HWND hwnd = CreateWindow(
+        wc.lpszClassName, // 利用するクラス名
+        L"CG2", // タイトルバーの文字
+        WS_OVERLAPPEDWINDOW, // よく見るウィンドウスタイル
+        CW_USEDEFAULT, // 座標X表示
+        CW_USEDEFAULT, // 座標Y表示
+        wrc.right - wrc.left, // ウィンドウ横幅
+        wrc.bottom - wrc.top, // ウィンドウ縦幅
+        nullptr, // メニュースタイル
+        nullptr, wc.hInstance, // インスタンスハンドル
+        nullptr); // オプション
+    // ウィンドウを表示する
+    ShowWindow(hwnd, SW_SHOW);
 }
 
 void WinApp::Update()
--- project/main.cpp
+++ project/main.cpp
@@ -793,7 +793,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
     D3DResourceLeakChecker leakChecl;
 
-    CoInitializeEx(0, COINIT_MULTITHREADED);
     // 誰も捕捉しなかった場合に、捕捉する関数を登録
     SetUnhandledExceptionFilter(ExportDump);
 
@@ -813,40 +812,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
     // ファイルを作って書き込み準備
     std::ofstream logStream(logFilePath);
 
-    WNDCLASS wc {};
-    // ウィンドウプロ―ジャ
-    wc.lpfnWndProc = Windowproc;
-    // ウィンドウクラス名(なんでもよし)
-    wc.lpszClassName = L"CG2WindowClass";
-    // インスタンスハンドル
-    wc.hInstance = GetModuleHandle(nullptr);
-    // カーソル
-    wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
-    // ウィンドウクラスを登録する
-    RegisterClass(&wc);
+ 
 
-    // クライアント領域のサイズ
-    const int32_t KClientWidth = 1280;
-    const int32_t KClientHeight = 720;
-
-    // ウィンドウサイズを表す構造体にクライアント領域を入れる
-    RECT wrc = { 0, 0, KClientWidth, KClientHeight };
-
-    // クライアント領域を元に実際のサイズをwrcを変更してもらう
-    AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
-
-    // ウィンドウの作成
-    HWND hwnd = CreateWindow(
-        wc.lpszClassName, // 利用するクラス名
-        L"CG2", // タイトルバーの文字
-        WS_OVERLAPPEDWINDOW, // よく見るウィンドウスタイル
-        CW_USEDEFAULT, // 座標X表示
-        CW_USEDEFAULT, // 座標Y表示
-        wrc.right - wrc.left, // ウィンドウ横幅
-        wrc.bottom - wrc.top, // ウィンドウ縦幅
-        nullptr, // メニュースタイル
-        nullptr, wc.hInstance, // インスタンスハンドル
-        nullptr); // オプション
+   
 
 #ifdef _DEBUG
     Microsoft::WRL::ComPtr<ID3D12Debug1> debugController = nullptr;
@@ -858,8 +826,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
     }
 #endif // _DEBUG
 
-    // ウィンドウを表示する
-    ShowWindow(hwnd, SW_SHOW);
     // 出力ウィンドウへの文字出力
     Log(logStream, "Hello,DirectX!\n");
     Log(logStream, ConvertString(std::format(L"clientSize{},{}\n", KClientWidth, KClientHeight)));
