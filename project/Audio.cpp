947a4d366079dc4debdbf25518deee0972ce5a4b
ikeda_takumi <zhuomichitisn03@gmail.com>
2026-01-09 10:46:00 +0900

    wavクラス化

---
+++ project/Audio.cpp
@@ -0,0 +1,40 @@
+#include "Audio.h"
+#include "Sound.h"
+#include <cassert>
+
+bool Audio::Initialize()
+{
+    HRESULT hr = XAudio2Create(&xAudio2_, 0);
+    if (FAILED(hr))
+        return false;
+
+    hr = xAudio2_->CreateMasteringVoice(&masterVoice_);
+    return SUCCEEDED(hr);
+}
+
+void Audio::Finalize()
+{
+    if (masterVoice_) {
+        masterVoice_->DestroyVoice();
+        masterVoice_ = nullptr;
+    }
+    xAudio2_.Reset();
+}
+
+void Audio::Play(const Sound& sound)
+{
+    IXAudio2SourceVoice* sourceVoice = nullptr;
+
+    HRESULT hr = xAudio2_->CreateSourceVoice(
+        &sourceVoice,
+        &sound.GetFormat());
+    assert(SUCCEEDED(hr));
+
+    XAUDIO2_BUFFER buf {};
+    buf.pAudioData = sound.GetBuffer();
+    buf.AudioBytes = sound.GetBufferSize();
+    buf.Flags = XAUDIO2_END_OF_STREAM;
+
+    sourceVoice->SubmitSourceBuffer(&buf);
+    sourceVoice->Start();
+}
