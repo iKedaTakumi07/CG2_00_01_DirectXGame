8441310de7a5b4eb4367ee904df3480648c3b95e
ikeda_takumi <zhuomichitisn03@gmail.com>
2026-06-16 12:34:33 +0900

    試作A

--- project/Engine/scene/TitleScene.cpp
+++ project/Engine/scene/TitleScene.cpp
@@ -66,9 +66,10 @@ void TitleScene::Initialize()
     // model_2->SetEvnTexturefilePath(skydox->GetTextureFilePath());
     object3d_2->SetModel(model_2.get());
     object3d_2->LoadAnimation("resources", "AnimatedCube.gltf", "test");
-    object3d_2->PlayAnimation("test", false);
+    object3d_2->PlayAnimation("test", true);
 
     ParticleManager::getInstance()->CreateParticleGroup("pori", "resources/circle2.png", ParticleMeshType::kPlane);
+    ParticleManager::getInstance()->CreateParticleGroup("circle3", "resources/circle3.png", ParticleMeshType::kPlane);
     ParticleManager::getInstance()->CreateParticleGroup("Plane", "resources/uvChecker.png", ParticleMeshType::kPlane);
     ParticleManager::getInstance()->CreateParticleGroup("gradationLine", "resources/gradationLine.png", ParticleMeshType::kRing);
     ParticleManager::getInstance()->CreateParticleGroup("Cylinder", "resources/gradationLine.png", ParticleMeshType::kCylinder);
@@ -82,32 +83,32 @@ void TitleScene::Initialize()
     emitter.translate = { 0.0f, 2.0f, 0.0f };
     emitter.rotate = { 0.0f, 0.0f, 1.0f };
     emitter.scale = { 0.05f, 1.0f, 1.0f };
-    particleEmitter = std::make_unique<ParticleEmitter>("pori", emitter, 1.0f, 8, true);
+    particleEmitter = std::make_unique<ParticleEmitter>("pori", emitter, 0.8f, 8, true);
     EmitterParam fireParam;
     fireParam.maxRotate = { 0.0f, 0.0f, std::numbers::pi_v<float> };
     fireParam.minRotate = { 0.0f, 0.0f, -std::numbers::pi_v<float> };
     fireParam.maxScale = { 0.05f, 1.5f, 1.0f };
     fireParam.minScale = { 0.05f, 0.4f, 1.0f };
-    fireParam.SetStartColor({ 1.0f, 0.0f, 1.0f, 1.0f });
-    fireParam.SetEndColor({ 1.0f, 1.0f, 1.0f, 1.0f });
+    fireParam.SetStartColor({ 1.0f, 1.0f, 0.5f, 1.0f });
+    fireParam.SetEndColor({ 1.0f, 1.0f, 1.0f, 0.0f });
     fireParam.SetVelocity({ 0.0f, 0.0f, 0.0f });
     fireParam.SetLifeTime(1.0f);
     particleEmitter->SetParam(fireParam);
 
-    particleEmitter2 = std::make_unique<ParticleEmitter>("gradationLine", emitter, 1.0f, 3, true);
+    particleEmitter2 = std::make_unique<ParticleEmitter>("gradationLine", emitter, 0.8f, 3, true);
     fireParam.maxRotate = { std::numbers::pi_v<float>, std::numbers::pi_v<float>, 0.0f };
     fireParam.minRotate = { -std::numbers::pi_v<float>, -std::numbers::pi_v<float>, 0.0f };
     fireParam.maxScale = { 1.0f, 1.0f, 1.0f };
     fireParam.minScale = { 1.0f, 0.4f, 1.0f };
-    fireParam.SetStartColor({ 0.0f, 1.0f, 1.0f, 1.0f });
-    fireParam.SetEndColor({ 1.0f, 1.0f, 1.0f, 1.0f });
+    fireParam.SetStartColor({ 1.0f, 1.0f, 0.5f, 1.0f });
+    fireParam.SetEndColor({ 1.0f, 1.0f, 1.0f, 0.0f });
     fireParam.SetVelocity({ 0.0f, 0.0f, 0.0f });
-    fireParam.SetLifeTime(1.0f);
+    fireParam.SetLifeTime(1.2f);
     particleEmitter2->SetParam(fireParam);
 
     // [アップデート予定]パーティクルPS閾値をCBuffer経由で設定できるようにする
     emitter.translate = { 0.0f, 0.0f, 0.0f };
-    particleEmitter3 = std::make_unique<ParticleEmitter>("Cylinder", emitter, 10.0f, 1, false);
+    particleEmitter4 = std::make_unique<ParticleEmitter>("Cylinder", emitter, 10.0f, 1, false);
     fireParam.SetScale({ 1.0f, 1.0f, 1.0f });
     fireParam.SetRotate({ 0.0f, 0.0f, 0.0f });
     fireParam.SetStartColor({ 1.0f, 1.0f, 1.0f, 1.0f });
@@ -115,8 +116,21 @@ void TitleScene::Initialize()
     fireParam.SetVelocity({ 0.0f, 0.0f, 0.0f });
     fireParam.SetLifeTime(10.0f);
     fireParam.isInfinite = true;
-    particleEmitter3->SetParam(fireParam);
+    particleEmitter4->SetParam(fireParam);
     ParticleManager::getInstance()->SetGroupScrollSpeed("Cylinder", { 0.2f, 0.0f });
+
+    emitter.translate = { 0.0f, 2.0f, 0.0f };
+    particleEmitter3 = std::make_unique<ParticleEmitter>("circle3", emitter, 0.8f, 32, true);
+    fireParam.maxRotate = { 0.0f, 0.0f, 0.0f };
+    fireParam.minRotate = { 0.0f, 0.0f, 0.0f };
+    fireParam.SetScale({ 0.02f, 0.02f, 0.02f });
+    fireParam.maxVelocity = { 2.0f, 2.0f, 2.0f };
+    fireParam.minVelocity = { -2.0f, -2.0f, -2.0f };
+    fireParam.SetStartColor({ 1.0f, 1.0f, 0.0f, 1.0f });
+    fireParam.SetEndColor({ 1.0f, 1.0f, 0.0f, 0.0f });
+    fireParam.isInfinite = false;
+    fireParam.SetLifeTime(1.2f);
+    particleEmitter3->SetParam(fireParam);
 }
 
 void TitleScene::Finalize()
@@ -143,6 +157,7 @@ void TitleScene::Update()
     particleEmitter->Update();
     particleEmitter2->Update();
     particleEmitter3->Update();
+    particleEmitter4->Update();
 
     // IMGUI
     object3d->DrawImGui("Terrain");
