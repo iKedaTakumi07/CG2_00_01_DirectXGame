7e3862498d7458260479f14fa0b891bcddb9821b
ikeda_takumi <zhuomichitisn03@gmail.com>
2026-07-16 15:17:55 +0900

    FreeList完成。

--- project/resources/shaders/InitializeParticle.CS.hlsl
+++ project/resources/shaders/InitializeParticle.CS.hlsl
@@ -10,7 +10,8 @@ struct Particle
 
 static const uint32_t kMaxParticles = 1024;
 RWStructuredBuffer<Particle> gParticles : register(u0);
-RWStructuredBuffer<int32_t> gFreeCounter : register(u1);
+RWStructuredBuffer<int32_t> gFreeListIndex : register(u1);
+RWStructuredBuffer<int32_t> gFreeList : register(u2);
 
 [numthreads(1024, 1, 1)]
 void main(uint32_t3 DTid : SV_DispatchThreadID)
@@ -24,9 +25,14 @@ void main(uint32_t3 DTid : SV_DispatchThreadID)
         gParticles[particleIndex].color = float32_t4(1.0f, 1.0f, 1.0f, 0.0f);
         gParticles[particleIndex].currentTime = 0.01f;
         gParticles[particleIndex].lifeTime = 2.0f;
+        if (particleIndex < kMaxParticles)
+        {
+            gParticles[particleIndex] = (Particle) 0;
+            gFreeList[particleIndex] = particleIndex;
+        }
         if (particleIndex == 0)
         {
-            gFreeCounter[0] = 0;
+            gFreeListIndex[0] = kMaxParticles - 1;
         }
     }
 
--- project/resources/shaders/UpdateParticle.CS.hlsl
+++ project/resources/shaders/UpdateParticle.CS.hlsl
@@ -16,6 +16,8 @@ struct PerFrame
 static const uint32_t kMaxParticles = 1024;
 ConstantBuffer<PerFrame> gPerFrame : register(b0);
 RWStructuredBuffer<Particle> gParticles : register(u0);
+RWStructuredBuffer<int32_t> gFreeListIndex : register(u1);
+RWStructuredBuffer<int32_t> gFreeList : register(u2);
 
 [numthreads(1024, 1, 1)]
 void main(uint3 DTid : SV_DispatchThreadID)
@@ -26,12 +28,30 @@ void main(uint3 DTid : SV_DispatchThreadID)
         // alphaが0のparticleは死んでいるとみなして更新しない
         if (gParticles[particleIndex].color.a != 0)
         {
+            // スケールに0を入れておいてvertexshader出力で忘却されるようにする
             gParticles[particleIndex].translate += gParticles[particleIndex].velocity;
             gParticles[particleIndex].currentTime += gPerFrame.deltaTime;
             float32_t alpha = 1.0f - (gParticles[particleIndex].currentTime / gParticles[particleIndex].lifeTime);
             gParticles[particleIndex].color.a = saturate(alpha);
-
+        }
+        
+        // alphaが0なのでFreeにする
+        if (gParticles[particleIndex].color.a == 0)
+        {
+            // スケールに0を入れておいてvertexshader出力で忘却されるようにする
+            gParticles[particleIndex].scale = float32_t3(0.0f, 0.0f, 0.0f);
+            int32_t freeListIndex;
+            InterlockedAnd(gFreeListIndex[0], 1, freeListIndex);
+            // 最新のfreeListindexの場所に死んだParticleのindexを設定する
+            if ((freeListIndex + 1) < kMaxParticles)
+            {
+                gFreeList[freeListIndex + 1] = particleIndex;
+            }
+            else
+            {
+                // 通るはずがない、万が一の安全策。
+                InterlockedAdd(gFreeListIndex[0], -1, freeListIndex);
+            }
         }
     }
-
 }
